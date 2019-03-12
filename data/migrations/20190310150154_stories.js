b7b1e960d6cec5da8bd5f0b3dce419b4fa3db46d
Ryan Walker <rytwalker@gmail.com>
2019-03-12 19:22:13 -0400

    foreign key update

--- data/migrations/20190310150154_stories.js
+++ data/migrations/20190310150154_stories.js
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
     tbl.string('story_country', 32).notNullable();
     tbl.string('story_user', 64).notNullable();
     tbl
-      .foreign('story_user')
+      .foreign('story_user', 'user_id')
       .references('id')
       .inTable('users');
     tbl.timestamp('create_date').defaultTo(knex.fn.now());
