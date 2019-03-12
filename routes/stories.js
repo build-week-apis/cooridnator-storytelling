979c1b44eecec7da9223e8b0c37a961bdc272676
ltrii <lelandtr@gmail.com>
2019-03-12 12:33:41 -0700

    Story routes fleshed out

--- routes/stories.js
+++ routes/stories.js
@@ -27,4 +27,52 @@ router.post('/', mdl.restricted, (req, res) => {
     });
 });
 
+router.get('/:id', mdl.restricted, async (req, res) => {
+  try {
+    const story = await db('stories')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(story);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put('/:id', mdl.restricted, async (req, res) => {
+  if(!req.body.username) {
+    res.status(400).json({ errormsg: 'Please enter a username' });
+    return;
+  }
+  try {
+    const count = await db('stories')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const story = await db('stories')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(story);
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
+
+
+router.delete('/:id', mdl.restricted, async (req, res) => {
+  try {
+    const count = await db('stories')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
+
 module.exports = router;
