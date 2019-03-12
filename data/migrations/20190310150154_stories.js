46fe24cdf0783993e8e24dc7d82ade28826b592d
Ryan Walker <rytwalker@gmail.com>
2019-03-12 18:29:38 -0400

    updates

--- data/migrations/20190310150154_stories.js
+++ data/migrations/20190310150154_stories.js
@@ -1,20 +1,18 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('stories', tbl => {
-        tbl.increments()
-        tbl.string("story_title")
-           .notNullable()
-        tbl.string("story_description")
-           .notNullable()
-        tbl.string("story_country")
-           .notNullable()
-        tbl.string("story_user")
-           .notNullable()
-        tbl.foreign("story_user").references("id").inTable('users')
-        tbl.timestamp('create_date').defaultTo(knex.fn.now());
-    })
+  return knex.schema.createTable('stories', tbl => {
+    tbl.increments();
+    tbl.string('story_title').notNullable();
+    tbl.string('story_description').notNullable();
+    tbl.string('story_country').notNullable();
+    tbl.string('story_user').notNullable();
+    tbl
+      .foreign('story_user')
+      .references('id')
+      .inTable('users');
+    tbl.timestamp('create_date').defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('stories')
+  return knex.schema.dropTableIfExists('stories');
 };
