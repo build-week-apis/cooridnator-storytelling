84a3efe9aff2d80ebfaf14a94efa119f47b07ec4
ltrii <lelandtr@gmail.com>
2019-03-13 10:35:14 -0700

    Migration update

--- data/migrations/20190310150154_stories.js
+++ data/migrations/20190310150154_stories.js
@@ -4,16 +4,13 @@ exports.up = function(knex, Promise) {
     tbl.string('story_title', 160).notNullable();
     tbl.text('story_description').notNullable();
     tbl.string('story_country', 32).notNullable();
-    //  tbl.string('story_user', 64).notNullable();
-    //  tbl
-    //    .foreign('story_user', 'user_id')
-    //    .references('id')
-    //    .inTable('users');
     tbl
       .integer('user_id')
       .unsigned()
       .references('id')
-      .inTable('users');
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
     tbl.timestamp('create_date').defaultTo(knex.fn.now());
   });
 };
