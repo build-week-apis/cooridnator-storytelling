b1168cda11c4692849b80b5339816eea6f835a87
ltrii <lelandtr@gmail.com>
2019-03-14 15:55:50 -0700

    Some tests

--- index.spec.js
+++ index.spec.js
@@ -12,5 +12,59 @@ describe('server.js', () => {
         });
     });
 
+    it('should return JSON', async () => {
+      const res = await request(server).get('/api/stories');
+
+      expect(res.type).toBe('application/json');
+    });
+
   });
+
+  describe('POST /api/register', () => {
+    it("should return 201", async () => {
+        const game = {
+          username: 'dood',
+          password: 'dood',
+          email: 'dood@leland.net',
+          country: 'peru',
+          title: 'Backend dude',
+          role: 'coordinator'
+          };
+        const response = await request(server)
+          .post("/api/register")
+          .send(game);
+        expect(response.status).toEqual(201);
+    })
+  })
+
+  
+  describe('POST /api/login', () => {
+    it("should return 201", async () => {
+        const game = {
+          password: 'dood',
+          email: 'dood@leland.net'
+          };
+        const response = await request(server)
+          .post("/api/login")
+          .send(game);
+        expect(response.status).toEqual(200);
+    })
+  })
+
+  // describe('GET /api/users', () => {
+  //   it('should return 200 OK', () => {
+  //     return request(server)
+  //       .get('/api/users')
+  //       .then(res => {
+  //         expect(res.status).toBe(200);
+  //       });
+  //   });
+
+  //   it('should return JSON', async () => {
+  //     const res = await request(server).get('/api/users');
+
+  //     expect(res.type).toBe('application/json');
+  //   });
+
+  // });
 }); 
